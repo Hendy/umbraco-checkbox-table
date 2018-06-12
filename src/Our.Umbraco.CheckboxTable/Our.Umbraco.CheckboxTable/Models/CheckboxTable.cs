3cfcbfce86ead86b78015372585c667f6077c6c7
Hendy Racher <git@hendyracher.co.uk>
2018-06-12 19:15:15 +0100

    migrated models

---
+++ src/Our.Umbraco.CheckboxTable/Our.Umbraco.CheckboxTable/Models/CheckboxTable.cs
@@ -0,0 +1,18 @@
+﻿namespace Our.Umbraco.CheckboxTable.Models
+{
+    /// <summary>
+    /// Model for the Checkbox Table property editor
+    /// </summary>
+    public class CheckboxTable
+    {
+        /// <summary>
+        /// Collection of collumn labels (the first empty th cell is not in this collection)
+        /// </summary>
+        public string[] ColumnLabels { get; set; }
+
+        /// <summary>
+        /// Collection of rows (the first row of column labels is not in this collection)
+        /// </summary>
+        public CheckboxTableRow[] Rows { get; set; }
+    }
+}
---
+++ src/Our.Umbraco.CheckboxTable/Our.Umbraco.CheckboxTable/Models/CheckboxTableRow.cs
@@ -0,0 +1,18 @@
+﻿namespace Our.Umbraco.CheckboxTable.Models
+{
+    /// <summary>
+    /// Represents a row in a checkbox table (but not the column heading row)
+    /// </summary>
+    public class CheckboxTableRow
+    {
+        /// <summary>
+        /// The first cell in a row is the row label
+        /// </summary>
+        public string RowLabel { get; set; }
+
+        /// <summary>
+        /// All cells (except the row label) are boolean flags (as respresented in the cms by checkboxes)
+        /// </summary>
+        public bool[] Cells { get; set; }
+    }
+}
