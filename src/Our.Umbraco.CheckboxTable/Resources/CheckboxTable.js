96cde512bafe223dca262da658b920533715cc2a
Hendy Racher <git@hendyracher.co.uk>
2018-06-27 19:36:24 +0100

    min of 1x1 table & refactored js

--- src/Our.Umbraco.CheckboxTable/Resources/CheckboxTable.js
+++ src/Our.Umbraco.CheckboxTable/Resources/CheckboxTable.js
@@ -11,16 +11,13 @@
     // this will become the main controller for both an editor connfig and the property editor
     function CheckboxTableController($scope) {
 
-   
-        // if this being used as the prevalue editor, create fake config
-        if (angular.equals($scope.model.config, {})) {
-            console.log('in config mode');
-
+        // if this being used as a prevalue editor then create a full fake config 
+        if (angular.equals($scope.model.config, {})) { // is there a better way to check ?
             $scope.model.config = {
                 defaultCheckboxTable: {
-                    columns: [],
-                    rows: [],
-                    cells: []
+                    columns: [''],
+                    rows: [''],
+                    cells: [[false]]
                 },
                 columnPermissions: 'addRemove',
                 rowPermissions: 'addRemove'
@@ -30,59 +27,51 @@
         // v0.2.0 saved array data, whilst v0.3.0 saves an object - here we clear any legacy saved values to prevent errors
         if (angular.isArray($scope.model.value)) { $scope.model.value = null; }
 
+        // ensure model.value exists
         $scope.model.value = $scope.model.value || $scope.model.config.defaultCheckboxTable;
-
-        // watch the columns collection and rows collection, and on change, update cells (so we can sort cols in future)
-        $scope.$watch('model.value.columns', function (newColumns, oldColumns) {
-            updateCells();
-        }, true); // NOTE: bug in Angular $watchCollection, doesn't return the old value correctly, so using deep $watch instead
-
-        $scope.$watch('model.value.rows', function (newRows, oldRows) { 
-            updateCells();
-        }, true); // NOTE: bug in Angular $watchCollection, doesn't return the old value correctly, so using deep $watch instead
 
         $scope.addColumn = function () {
             $scope.model.value.columns.push('');
+            updateCells();
         };
 
         $scope.removeColumn = function (columnIndex) {
-            $scope.model.value.columns.splice(columnIndex, 1);
+            if ($scope.model.value.columns.length > 1) {
+                $scope.model.value.columns.splice(columnIndex, 1);
+                updateCells();
+            }
         };
 
         $scope.addRow = function () {
             $scope.model.value.rows.push('');
+            updateCells();
         };
 
         $scope.removeRow = function (rowIndex) {
-            $scope.model.value.rows.splice(rowIndex, 1);
+            if ($scope.model.value.rows.length > 1) {
+                $scope.model.value.rows.splice(rowIndex, 1);
+                updateCells();
+            }
         };
 
-        // function to look at the rows / columns and ensure the multi-dimension cells array is of the correct size
+        // ensure the multi-dimension cells array matches the rows & columns
         function updateCells() {
-            
+
             var rowCount = $scope.model.value.rows.length;
             var columnCount = $scope.model.value.columns.length;
-
-            var cells = new Array($scope.model.value.rows.length);
+            var cells = new Array(rowCount);
 
             for (var rowCounter = 0; rowCounter < rowCount; rowCounter ++)
             {
-                cells[rowCounter] = resizeArray($scope.model.value.cells[rowCounter], columnCount, false);
+                var checkboxes = $scope.model.value.cells[rowCounter] || new Array();
+
+                while (checkboxes.length > columnCount) { checkboxes.pop(); }
+                while (checkboxes.length < columnCount) { checkboxes.push(false); }
+
+                cells[rowCounter] = checkboxes;
             }
 
             $scope.model.value.cells = cells;
-        }
-
-        function resizeArray(array, newSize, newItem) {
-
-            if (!angular.isArray(array)) {
-                array = new Array();
-            };
-
-            while (array.length > newSize) { array.pop(); }
-            while (array.length < newSize) { array.push(newItem); }
-
-            return array;
         }
     }
 
